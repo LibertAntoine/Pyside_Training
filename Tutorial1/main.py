c1cf77a2dc1497fc65d7fbbfc805564555acef1c
AntoineLibert <duncan-arrakis@hotmail.fr>
2021-03-27 09:27:15 +0100

    [Tutorial1] Use and manipulate icons

--- Tutorial1/main.py
+++ Tutorial1/main.py
@@ -13,9 +13,15 @@ class Window(QWidget): # Hérite de la class QWidget
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Pyside2 Test") #Configure le titre de la fenêtre
-        self.setGeometry(300, 300, 300, 300) #Configure la taille de la fenêtre
+        self.setWindowTitle("Pyside2 Window") #Configure le titre de la fenêtre
 
+        # Setup Window
+        self.setGeometry(300, 300, 300, 300) #Configure la taille de la fenêtre
+        self.setMinimumHeight(100)
+        self.setMinimumWidth(250)
+
+        self.setMaximumHeight(200)
+        self.setMaximumWidth(800)
 
 myApp = QApplication(sys.argv)
 window = Window()
