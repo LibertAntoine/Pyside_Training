756d9cf8dc1f60c8c48a2fd02bd4977d6549fee1
AntoineLibert <duncan-arrakis@hotmail.fr>
2021-03-30 14:08:37 +0200

    [TestPyQt5] Config PyQt5 basic env

--- TestPyQt5/main.py
+++ TestPyQt5/main.py
@@ -1,18 +1,11 @@
-import sys,os
-import PySide2
-from PySide2.QtWidgets import QApplication, QWidget
-
-# Allow to find plugins needed for PySide2
-dirname = os.path.dirname(PySide2.__file__)
-plugin_path = os.path.join(dirname, 'plugins', 'platforms')
-os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
-
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
 
 class Window(QWidget): # Hérite de la class QWidget
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Pyside2 Window") #Configure le titre de la fenêtre
+        self.setWindowTitle("PyQt5 Window") #Configure le titre de la fenêtre
 
         # Setup Window
         self.setGeometry(300, 300, 600, 300) #Configure la taille de la fenêtre
